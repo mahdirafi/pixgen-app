efdd60620152425685f6cc4671aa6c939c9f7611
naim <mdnaimi277@gmail.com>
2026-06-25 23:31:02 +0600

    final project deploy

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000"
+  baseURL: "https://pixgen-app.vercel.app"
 });
 
 
